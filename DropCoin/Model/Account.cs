d863d57090fb575203c15693751e024e510fab79
TheZan <sochnev66@gmail.com>
2020-10-10 22:09:52 +0300

    Add registration and login tabs

---
+++ DropCoin/Model/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.Hex.HexTypes;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts.Managed;
+
+namespace DropCoin.Model
+{
+    public static class Account
+    {
+        private const string RpcUrl = "http://127.0.0.1:8545";
+        public static string AccountAddress { get; set; }
+        public static string AccountPassword { get; set; }
+        private static Web3 web3 { get; set; }
+
+        public static void Login()
+        {
+            try
+            {
+                var account = new ManagedAccount(AccountAddress, AccountPassword);
+                web3 = new Web3(account, RpcUrl);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public static async void Registration(string password)
+        {
+            try
+            {
+                web3 = new Web3(RpcUrl);
+                //var test = await web3.Personal.NewAccount.SendRequestAsync(password);
+                //MessageBox.Show(test);
+
+                var ecKey = Nethereum.Signer.EthECKey.GenerateKey();
+                var privateKey = ecKey.GetPrivateKeyAsBytes().ToHex();
+                var account = new Nethereum.Web3.Accounts.Account("tre");
+                var t = account.PrivateKey;
+                MessageBox.Show(t);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public static async Task<string> GetBalance()
+        {
+            try
+            {
+                var balance = await web3.Eth.GetBalance.SendRequestAsync(AccountAddress);
+                return balance.Value.ToString();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+    }
+}
--- DropCoin/View/StartWindow.xaml.cs
+++ DropCoin/View/StartWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DropCoin.Model;
+using Microsoft.Win32;
 
 namespace DropCoin.View
 {
@@ -25,6 +27,29 @@ namespace DropCoin.View
         private void CloseWindow_Event(object sender, MouseButtonEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void SelectPathButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == true)
+            {
+                KeyStorePath.Text = open.FileName;
+            }
+        }
+
+        private async void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Account.AccountAddress = "0x29d569e77EA47600634d30e3A71a9e4fe63E60bb";
+            Account.AccountPassword = "test";
+            Account.Login();
+            MessageBox.Show(await Account.GetBalance());
+        }
+
+        private void CreateAccount_OnClick(object sender, RoutedEventArgs e)
+        {
+            Account.Registration(RegistrationPassword.Password);
+            RegistrationPassword.Clear();
         }
     }
 }
